edb409cd12c73332baac0bc57ef8ea588f185d75
Lara Martin <lara.martin.carretero@gmail.com>
2016-08-25 21:25:10 +0200

    activities linked from menu

--- app/src/main/java/eu/laramartin/musicplayer/MainActivity.java
+++ app/src/main/java/eu/laramartin/musicplayer/MainActivity.java
@@ -19,10 +19,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        playerTextView = (TextView) findViewById(R.id.menuPlaylists);
+        playlistsTextView = (TextView) findViewById(R.id.menuPlayer);
         albumsTextView = (TextView) findViewById(R.id.menuAlbums);
+        settingsTextView = (TextView) findViewById(R.id.menuSettings);
+
+        playerTextView.setOnClickListener(playerTextViewOnClickListener);
+        playlistsTextView.setOnClickListener(playlistsTextViewOnClickListener);
         albumsTextView.setOnClickListener(albumsTextViewOnClickListener);
+        settingsTextView.setOnClickListener(settingsTextViewOnClickListener);
 
     }
+
+    final View.OnClickListener playlistsTextViewOnClickListener = new View.OnClickListener(){
+        public void onClick(final View v){
+            openPlaylistsList(playlistsTextView);
+        }
+    };
+
+    final View.OnClickListener playerTextViewOnClickListener = new View.OnClickListener(){
+        public void onClick(final View v){
+            openPlayerList(playerTextView);
+        }
+    };
 
     final View.OnClickListener albumsTextViewOnClickListener = new View.OnClickListener(){
         public void onClick(final View v){
@@ -30,8 +49,29 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    final View.OnClickListener settingsTextViewOnClickListener = new View.OnClickListener(){
+        public void onClick(final View v){
+            openSettingsList(settingsTextView);
+        }
+    };
+
     public void openAlbumsList(View view) {
         Intent intent = new Intent(this, AlbumsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPlayerList(View view) {
+        Intent intent = new Intent(this, PlayerActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPlaylistsList(View view) {
+        Intent intent = new Intent(this, PlaylistsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSettingsList(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 
