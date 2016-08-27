acb8a02d3b5beb805918b5fe9a1dbb4174a76d1e
Lara Martin <lara.martin.carretero@gmail.com>
2016-08-27 14:29:09 +0200

    open menu from player

--- app/src/main/java/eu/laramartin/musicplayer/PlayerActivity.java
+++ app/src/main/java/eu/laramartin/musicplayer/PlayerActivity.java
@@ -1,13 +1,32 @@
 package eu.laramartin.musicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PlayerActivity extends AppCompatActivity {
+
+    ImageView playerMenuView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        playerMenuView = (ImageView) findViewById(R.id.playerMenuView);
+        playerMenuView.setOnClickListener(playerMenuViewOnClickListener);
+    }
+
+    final View.OnClickListener playerMenuViewOnClickListener = new View.OnClickListener(){
+        public void onClick(final View v){
+            openMenu(playerMenuView);
+        }
+    };
+
+    public void openMenu(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
